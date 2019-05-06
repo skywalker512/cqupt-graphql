1493f04242c2e050f8e78ff87bc233d45f1190cb
houzhenhong <houzhenhong@hotmail.com>
2019-05-06 23:02:38 +0800

    chore(*): webpack 热更新

--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,18 @@ import {
   NestFastifyApplication,
 } from '@nestjs/platform-fastify';
 
+declare const module: any;
+
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter());
   // const app = await NestFactory.create<NestExpressApplication>(ApplicationModule);
   // 将类型传递给 NestFactory.create() 方法时，如下例所示，app 对象将具有专用于该特定平台的方法。 但是，请注意，除非您确实要访问底层平台API，否则无需指定类型。
   // app.useGlobalFilters(new HttpExceptionFilter());
   await app.listen(3000);
+
+  if (module.hot) {
+    module.hot.accept();
+    module.hot.dispose(() => app.close());
+  }
 }
 bootstrap();
