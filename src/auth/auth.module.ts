b51f2187513945d0161bf546e9dffd3fbf25b26f
houzhenhong <houzhenhong@hotmail.com>
2019-05-10 17:07:57 +0800

    feat(auth): 自己写验证逻辑

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,22 +1,8 @@
 import { Module } from '@nestjs/common';
-import { PassportModule } from '@nestjs/passport';
-import { JwtModule } from '@nestjs/jwt';
-import { UsersModule } from '@/src/users/users.module';
 import { AuthService } from './auth.service';
-import { JwtStrategy } from './jwt.strategy';
 
 @Module({
-  imports: [
-    PassportModule.register({ defaultStrategy: 'jwt' }),
-    JwtModule.register({
-      secretOrPrivateKey: 'ddd',
-      signOptions: {
-        expiresIn: 3600,
-      },
-    }),
-    UsersModule,
-  ],
-  providers: [AuthService, JwtStrategy],
-  exports: [PassportModule, AuthService],
+  providers: [AuthService],
+  exports: [AuthService],
 })
 export class AuthModule {}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,21 +1,47 @@
-import { Injectable } from '@nestjs/common';
-import { UsersService } from '@/src/users/users.service';
-import { JwtService } from '@nestjs/jwt';
-import { JwtPayload } from './interfaces/jwt-payload.interface';
+import { Injectable, OnModuleInit, Inject } from '@nestjs/common';
+import { cqupt_user } from '../grpc/generated';
+import { GrpcClientFactory } from '../grpc/grpc.client-factory';
+import { AuthenticationError } from 'apollo-server-core';
+import * as jwt from 'jsonwebtoken';
 
 @Injectable()
-export class AuthService {
+export class AuthService implements OnModuleInit {
+  private userService: cqupt_user.UserController
   constructor(
-    private readonly usersService: UsersService,
-    private readonly jwtService: JwtService,
-  ) {}
-
-  async signIn(): Promise<string> {
-    const user: JwtPayload = { email: '11' }
-    return this.jwtService.sign(user)
+    @Inject(GrpcClientFactory) private readonly grpcClientFactory: GrpcClientFactory
+  ) { }
+  onModuleInit() {
+    this.userService = this.grpcClientFactory.userModuleClient.getService('UserController')
   }
 
-  async validateUser(payload: JwtPayload): Promise<any> {
-    return await this.usersService.findOneByEmail(payload.email)
+  async validateUser(req: any) {
+    /**
+     * whitelist
+     */
+    if (req.body && (['IntrospectionQuery', 'sayHello', 'login', 'adminLogin', 'register'].some(item => req.body.query.includes(item)))) {
+      return;
+    }
+
+    let token = req.headers.authorization as string;
+    if (!token) {
+      throw new AuthenticationError('请求头中应该有 Authorization 字段');
+    }
+
+    if (token.slice(0, 6) === 'Bearer') {
+      token = token.slice(7);
+    } else {
+      throw new AuthenticationError('请求头的 Authorization 字段的前缀应该是 Bearer');
+    }
+
+    try {
+      const decodedToken = <{ loginName: string }>jwt.verify(token, 'secretKey');
+    } catch (error) {
+      if (error instanceof jwt.JsonWebTokenError) {
+        throw new AuthenticationError('Authorization code 出错');
+      }
+      if (error instanceof jwt.TokenExpiredError) {
+        throw new AuthenticationError('Authorization code 过期');
+      }
+    }
   }
 }
